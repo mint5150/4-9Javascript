d2009d2c69bf077634ae8d77ab43ed3dbbc68fc8
mint5150 <musia.home@gmail.com>
2022-12-29 00:35:54 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var boolean = true;
+var resultText = boolean ? "Aの処理" : "Bの処理";
+console.log(resultText);
+
+var string = 6;
+var resultNum = string > 5  ? true : false;
+console.log(resultNum);
+
+var string = 4;
+var resultNum = string < 5  ? true : false;
+console.log(resultNum);
+
+var string = 10;
+var resultNum = string >= 10  ? true : false;
+console.log(resultNum);
+
+var string = "Javascript";
+var resultNum = !"" ? true : false;
+console.log(resultNum);
